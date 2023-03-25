ff35d9955f827d20c3549b92fc369ab493abaf10
Julio Cotzojay <juliocotzo11@gmail.com>
2023-03-25 08:06:49 -0600

    feat: Enable CORS

--- mp-backend/src/main/java/com/example/mpbackend/controller/LocationController.java
+++ mp-backend/src/main/java/com/example/mpbackend/controller/LocationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class LocationController {
